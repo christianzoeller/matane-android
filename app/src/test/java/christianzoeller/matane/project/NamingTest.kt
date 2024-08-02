4db7a5e16e6f20f46bfac6f2b3a75af7ee417c73
Christian Zöller <8214446-christianzoeller@users.noreply.gitlab.com>
2024-08-02 11:01:39 +0200

    Add Konsist tests

---
+++ app/src/test/java/christianzoeller/matane/project/NamingTest.kt
@@ -0,0 +1,41 @@
+package christianzoeller.matane.project
+
+import androidx.lifecycle.ViewModel
+import com.lemonappdev.konsist.api.Konsist
+import com.lemonappdev.konsist.api.ext.list.withAllParentsOf
+import com.lemonappdev.konsist.api.ext.list.withAnnotation
+import com.lemonappdev.konsist.api.ext.list.withPackage
+import com.lemonappdev.konsist.api.ext.list.withParameter
+import com.lemonappdev.konsist.api.verify.assertTrue
+import org.junit.Test
+
+class NamingTest {
+    @Test
+    fun `top-level screen composables have correct suffix`() {
+        // Here, we define top-level screen composables as those
+        // composables that have a view model parameter
+        Konsist.scopeFromProject()
+            .functions()
+            .withAnnotation { annotation -> annotation.name == "Composable" }
+            .withParameter { parameter ->
+                parameter.hasType { type -> type.hasNameEndingWith(viewModelSuffix) }
+            }
+            .assertTrue { it.name.endsWith(topLevelScreenComposableSuffix) }
+    }
+
+    @Test
+    fun `view models have correct suffix`() {
+        Konsist.scopeFromProject()
+            .classes()
+            .withAllParentsOf(ViewModel::class)
+            .assertTrue { it.name.endsWith(viewModelSuffix) }
+    }
+
+    @Test
+    fun `repositories have correct suffix`() {
+        Konsist.scopeFromProject()
+            .classes()
+            .withPackage(repositoryPackage)
+            .assertTrue { it.name.endsWith(repositorySuffix) }
+    }
+}
---
+++ app/src/test/java/christianzoeller/matane/project/Packages.kt
@@ -0,0 +1,8 @@
+package christianzoeller.matane.project
+
+const val basePackage = "christianzoeller.matane"
+const val featurePackages = "$basePackage.feature.."
+const val repositoryPackage = "$basePackage.repository"
+const val repositoryPackages = "$basePackage.repository.."
+const val datasourcePackage = "$basePackage.datasource"
+const val datasourcePackages = "$basePackage.datasource.."
