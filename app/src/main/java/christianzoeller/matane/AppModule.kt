3ad28047a02eb57194e6861d3fea7a438dadd4ab
Christian Zöller <8214446-christianzoeller@users.noreply.gitlab.com>
2024-08-02 11:13:01 +0200

    Setup Hilt and add common functionality

---
+++ app/src/main/java/christianzoeller/matane/AppModule.kt
@@ -0,0 +1,23 @@
+package christianzoeller.matane
+
+import christianzoeller.matane.annotation.ApplicationScope
+import christianzoeller.matane.annotation.DefaultDispatcher
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Singleton
+    @ApplicationScope
+    @Provides
+    fun provideApplicationScope(
+        @DefaultDispatcher defaultDispatcher: CoroutineDispatcher
+    ): CoroutineScope = CoroutineScope(SupervisorJob() + defaultDispatcher)
+}
---
+++ app/src/main/java/christianzoeller/matane/annotation/CoroutineScope.kt
@@ -0,0 +1,11 @@
+package christianzoeller.matane.annotation
+
+import javax.inject.Qualifier
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class DefaultDispatcher
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class ApplicationScope
