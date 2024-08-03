613ef99ae727cf501b1dbd75c61757bb048d7dcb
Christian Zöller <8214446-christianzoeller@users.noreply.gitlab.com>
2024-08-03 15:20:40 +0200

    Set navigation bar style to transparent

--- app/src/main/java/christianzoeller/matane/MainActivity.kt
+++ app/src/main/java/christianzoeller/matane/MainActivity.kt
@@ -1,7 +1,9 @@
 package christianzoeller.matane
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import christianzoeller.matane.ui.theme.MataneTheme
@@ -11,7 +13,12 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            navigationBarStyle = SystemBarStyle.auto(
+                lightScrim = Color.TRANSPARENT,
+                darkScrim = Color.TRANSPARENT
+            )
+        )
         setContent {
             val appState = rememberMataneAppState()
 
