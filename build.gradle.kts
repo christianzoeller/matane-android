3c22eb7c8bad96b5d0809896c80ef48022ca0c1b
Christian Zöller <scat.07.tresen@icloud.com>
2024-08-16 14:15:30 +0200

    Add dependencies for Firebase and enable Crashlytics

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.about.libraries)
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.crashlytics) apply false
+    alias(libs.plugins.google.services) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.hilt.android) apply false
     alias(libs.plugins.kotlinx.parcelize) apply false
